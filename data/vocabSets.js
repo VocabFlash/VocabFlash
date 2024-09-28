b7c64776e815de4e3101d9694082d1971876010d
kynmjl <keyan.mujalo@stud.altekanti.ch>
2024-09-28 17:55:57 +0200

    Update vocabSets.js

--- data/vocabSets.js
+++ data/vocabSets.js
@@ -1,1 +1,23 @@
+const vocabSets = {
+    set1: [
+        { de: "Beweis", ar: "إثبات" },
+        { de: "Vater", ar: "أب" },
+        { de: "mein Vater", ar: "أبي" },
+    ],
+    set2: [
+        { de: "Schwester", ar: "أخت" },
+        { de: "Tür", ar: "باب" },
+        { de: "Krone", ar: "تاج" },
+    ],
+    set3: [
+        { de: "und", ar: "و" },
+        { de: "Suche", ar: "بحث" },
+        { de: "ich suche", ar: "أبحث" },
+    ]
+};
+
+// Funktion, um das gewählte Set basierend auf dem Namen zurückzugeben
+function getVocabSet(setName) {
+    return vocabSets[setName];
+}
 
