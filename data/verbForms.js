3d49e5d0113790f3920ba95300d39e9d2513547b
kynmjl <keyan.mujalo@stud.altekanti.ch>
2024-09-30 18:39:08 +0200

    Update verbForms.js

--- data/verbForms.js
+++ data/verbForms.js
@@ -1,1 +1,41 @@
+const verbSets = {
+    set1: {
+        "parler": {
+            present: {
+                je: "parle",
+                tu: "parles",
+                il: "parle",
+                elle: "parle",
+                nous: "parlons",
+                vous: "parlez",
+                ils: "parlent",
+                elles: "parlent"
+            },
+            passeCompose: {
+                je: "ai parlé",
+                tu: "as parlé",
+                il: "a parlé",
+                elle: "a parlé",
+                nous: "avons parlé",
+                vous: "avez parlé",
+                ils: "ont parlé",
+                elles: "ont parlé"
+            },
+            imparfait: {
+                je: "parlais",
+                tu: "parlais",
+                il: "parlait",
+                elle: "parlait",
+                nous: "parlions",
+                vous: "parliez",
+                ils: "parlaient",
+                elles: "parlaient"
+            }
+        },
+        "marcher": { /* ähnliche Struktur wie bei 'parler' */ },
+        "aimer": { /* ähnliche Struktur wie bei 'parler' */ },
+        // Weitere Verben hier...
+    }
+    // Weitere Sets hier...
+};
 
