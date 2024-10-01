7646393871a8723ffca27386e82623e338326f6d
kynmjl <keyan.mujalo@stud.altekanti.ch>
2024-10-01 10:21:48 +0200

    Update verbForms.js

--- data/verbForms.js
+++ data/verbForms.js
@@ -1,41 +1,21 @@
+// Beispiel: verbForms.js
 const verbSets = {
-    set1: {
-        "parler": {
-            present: {
-                je: "parle",
-                tu: "parles",
-                il: "parle",
-                elle: "parle",
-                nous: "parlons",
-                vous: "parlez",
-                ils: "parlent",
-                elles: "parlent"
-            },
-            passeCompose: {
-                je: "ai parlé",
-                tu: "as parlé",
-                il: "a parlé",
-                elle: "a parlé",
-                nous: "avons parlé",
-                vous: "avez parlé",
-                ils: "ont parlé",
-                elles: "ont parlé"
-            },
-            imparfait: {
-                je: "parlais",
-                tu: "parlais",
-                il: "parlait",
-                elle: "parlait",
-                nous: "parlions",
-                vous: "parliez",
-                ils: "parlaient",
-                elles: "parlaient"
-            }
+    set1: [
+        {
+            de: "machen", 
+            present: { je: "je fais", tu: "tu fais", il: "il fait", nous: "nous faisons", vous: "vous faites", ils: "ils font" },
+            passeCompose: { je: "j'ai fait", tu: "tu as fait", il: "il a fait", nous: "nous avons fait", vous: "vous avez fait", ils: "ils ont fait" },
+            imparfait: { je: "je faisais", tu: "tu faisais", il: "il faisait", nous: "nous faisions", vous: "vous faisiez", ils: "ils faisaient" }
         },
-        "marcher": { /* ähnliche Struktur wie bei 'parler' */ },
-        "aimer": { /* ähnliche Struktur wie bei 'parler' */ },
-        // Weitere Verben hier...
-    }
-    // Weitere Sets hier...
+        // Weitere Verben
+    ],
+    set2: [
+        {
+            de: "gehen", 
+            present: { je: "je vais", tu: "tu vas", il: "il va", nous: "nous allons", vous: "vous allez", ils: "ils vont" },
+            passeCompose: { je: "je suis allé", tu: "tu es allé", il: "il est allé", nous: "nous sommes allés", vous: "vous êtes allés", ils: "ils sont allés" },
+            imparfait: { je: "j'allais", tu: "tu allais", il: "il allait", nous: "nous allions", vous: "vous alliez", ils: "ils allaient" }
+        }
+        // Weitere Verben
+    ]
 };
-
