16621addbf4e21771e743bb3282eb37860cfb379
kynmjl <keyan.mujalo@stud.altekanti.ch>
2024-10-22 20:29:29 +0200

    Update vocaBlanks.js

--- data/vocaBlanks.js
+++ data/vocaBlanks.js
@@ -1,9 +1,56 @@
 const vocabSets = {
   set1: [
-    { de: "He likes ... .", ar: "apples" },
-    { de: "She has ... .", ar: "a dog" },
-    // Weitere Einträge
+      // Present Simple
+      { de: "She _____ (go, Present Simple) to the gym every day.", ar: "goes" },
+      { de: "The sun _____ (rise, Present Simple) in the east.", ar: "rises" },
+      { de: "They _____ (not, play, Present Simple) video games on weekdays.", ar: "do not play" },
+  
+      // Present Continuous
+      { de: "He _____ (write, Present Continuous) an email at the moment.", ar: "is writing" },
+      { de: "We _____ (not, listen, Present Continuous) to music right now.", ar: "are not listening" },
+      { de: "I _____ (cook, Present Continuous) dinner while you are out.", ar: "am cooking" },
+  
+      // Past Simple
+      { de: "They _____ (visit, Past Simple) their grandparents last weekend.", ar: "visited" },
+      { de: "She _____ (not, finish, Past Simple) her project on time.", ar: "did not finish" },
+      { de: "He _____ (take, Past Simple) the wrong bus yesterday.", ar: "took" },
+  
+      // Past Continuous
+      { de: "I _____ (watch, Past Continuous) TV when you called.", ar: "was watching" },
+      { de: "They _____ (not, play, Past Continuous) outside because it was raining.", ar: "were not playing" },
+      { de: "We _____ (talk, Past Continuous) about the movie when she arrived.", ar: "were talking" },
+  
+      // Present Perfect
+      { de: "She _____ (see, Present Perfect) that film three times.", ar: "has seen" },
+      { de: "They _____ (not, finish, Present Perfect) their homework yet.", ar: "have not finished" },
+      { de: "I _____ (read, Present Perfect) five books this month.", ar: "have read" },
+  
+      // Past Perfect
+      { de: "He _____ (leave, Past Perfect) before I arrived.", ar: "had left" },
+      { de: "They _____ (not, see, Past Perfect) that movie before.", ar: "had not seen" },
+      { de: "I _____ (finish, Past Perfect) my work by the time the meeting started.", ar: "had finished" },
+  
+      // Future Simple
+      { de: "I _____ (call, Future Simple) you tomorrow.", ar: "will call" },
+      { de: "She _____ (not, go, Future Simple) to the party next week.", ar: "will not go" },
+      { de: "They _____ (visit, Future Simple) us next month.", ar: "will visit" },
+  
+      // Future Continuous
+      { de: "This time tomorrow, I _____ (travel, Future Continuous) to Paris.", ar: "will be traveling" },
+      { de: "They _____ (not, work, Future Continuous) on the project next week.", ar: "will not be working" },
+      { de: "We _____ (celebrate, Future Continuous) our anniversary at this time next year.", ar: "will be celebrating" },
+  
+      // Present Perfect Continuous
+      { de: "She _____ (work, Present Perfect Continuous) here since 2020.", ar: "has been working" },
+      { de: "They _____ (not, study, Present Perfect Continuous) for the exam.", ar: "have not been studying" },
+      { de: "I _____ (run, Present Perfect Continuous) for an hour when it started to rain.", ar: "have been running" },
+  
+      // Past Perfect Continuous
+      { de: "He _____ (wait, Past Perfect Continuous) for an hour before she arrived.", ar: "had been waiting" },
+      { de: "They _____ (not, play, Past Perfect Continuous) the game long before it was released.", ar: "had not been playing" },
+      { de: "I _____ (work, Past Perfect Continuous) on the project for weeks before I submitted it.", ar: "had been working" },
   ],
+
   set2: [
     { de: "They enjoy ... .", ar: "swimming" },
     // Weitere Einträge
